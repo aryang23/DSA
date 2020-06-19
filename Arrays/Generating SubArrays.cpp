0a9f66fe195788040c98a4e9d0e13b7e266e90e6
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2020-06-20 02:56:42 +0530

    Generating Subarrays

---
+++ Arrays/Generating SubArrays.cpp
@@ -0,0 +1,29 @@
+//By Aryan Grover (@aryang23)
+//Generating SubArrays
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i;j<n;j++)
+		{
+			for(int k=i;k<=j;k++)
+			{
+				cout<<a[k];
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+	}
+}
+
+//By Aryan Grover (@aryang23)
+//Very Basic Approach for Generating Subarrays
